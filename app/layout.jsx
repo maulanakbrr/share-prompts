40dd7acfcddb10e66d227e57eefa128031d7c169
Akbar Maulana <amaulanaa995@gmail.com>
2023-07-20 20:18:07 +0700

    updated .env, layout and page. created Feed, Form, Nav, Profile, PromptCard and Provider components. created route.js for auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import '@styles/globals.css'
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
 
 export const metadata = {
   title: 'Shareprompts',
@@ -9,13 +11,16 @@ const RootLayout = ({children}) => {
   return (
     <html lang='en'>
       <body>
-        <div className="main">
-          <div className="gradient"/>
-        </div>
+        <Provider>
+          <div className="main">
+            <div className="gradient"/>
+          </div>
 
-        <main className="app">
-          {children}
-        </main>
+          <main className="app">
+            <Nav/>
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   )
