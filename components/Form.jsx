369f8f56db625bce3b1a1fc1a0134586fadb24ee
Akbar Maulana <amaulanaa995@gmail.com>
2023-08-01 10:55:36 +0700

    created Prompt model, prompt route and create-prompt page. modified Form component

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import Link from "next/link"
 
-const Form = () => {
+const Form = ({type, post, setPost, submitting, setSubmitting, handleSubmit}) => {
+  const { prompt, tag } = post
+
+  const handleFormChange = e => {
+    const { name, value } = e.target
+    setPost({
+      ...post,
+      [name]: value
+    })
+  }
+
   return (
-    <div>Form</div>
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share amazing prompt. 
+      </p>
+      <form onSubmit={handleSubmit} className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism">
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">Your AI Prompt</span>
+          <textarea 
+            value={prompt}
+            name="prompt" 
+            id="prompt" 
+            onChange={handleFormChange}
+            placeholder="Write your prompt here..." 
+            className="form_textarea"
+            required
+          />
+        </label>
+
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Tag {' '}
+            <span className="font-normal">&#40;#webdevelopment, #happy, #idea&#41;</span>
+          </span>
+          <input 
+            value={tag}
+            name="tag" 
+            id="tag" 
+            onChange={handleFormChange}
+            placeholder="#tag" 
+            className="form_input"
+            required
+          />
+        </label>
+
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <Link href='/' className="text-gray-500 text-sm">Cancel</Link>
+          <button 
+            type='submit'
+            disabled={submitting}
+            className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
+          >
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
+      </form>
+    </section>
   )
 }
 
